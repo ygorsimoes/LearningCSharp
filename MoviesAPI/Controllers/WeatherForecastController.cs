814431cb0593f79fcf1b4062b73120885c64853c
YGÃO <contato.ygorsimoes@gmail.com>
2022-10-07 11:25:18 -0300

    Realocação de pastas e arquivos para o .NET 6

--- MoviesAPI/Controllers/WeatherForecastController.cs
+++ MoviesAPI/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace FilmesAPI.Controllers;
+namespace MoviesAPI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- MoviesAPI/WeatherForecast.cs
+++ MoviesAPI/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace FilmesAPI;
+namespace MoviesAPI;
 
 public class WeatherForecast
 {
